a22994a610244c296b2d926633af0d5a407419a3
claridiva2000 <claridiva2000@gmail.com>
2019-01-26 15:15:31 -0600

    i hate callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,61 +1,39 @@
-// // ==== Callbacks ====  
+// ==== Callbacks ====  
 
-// /* Step 1: Create a higher-order function that accepts a callback
-//   * Create a higher-order function named consume that can take 3 parameters.
-//   * The first two parameters can accept any argument
-//   * The last parameter accepts a callback 
-//   * In the body of the function return the callback with the two parameters that you created
-// */
-// function consume (x, y, cb) {
-//   return cb(x+y)
-//  }
+/* Step 1: Create a higher-order function that accepts a callback
+  * Create a higher-order function named consume that can take 3 parameters.
+  * The first two parameters can accept any argument
+  * The last parameter accepts a callback 
+  * In the body of the function return the callback with the two parameters that you created
+*/
+function doHomework(subject, callback) {
+  console.log(`Starting my ${subject} homework.`);
+  callback();
+}
+function alertFinished(){
+  console.log('Finished my homework');
+}
 
-// consume (2, 3, function(consume){
-//   console.log(consume);
-// });
+doHomework('math', alertFinished);
 
-// /* Step 2: Create several functions to callback with consume();
-//   * Create a function named add that returns the sum of two numbers
-//   * Create a function named multiply that returns the product of two numbers 
-//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-// */
-// function add (x, y, cb) {
-//   return cb(x+y)
-//  }
-//  consume (x, y, function(add){
-//   console.log(add);
-// });
+/* Step 2: Create several functions to callback with consume();
+  * Create a function named add that returns the sum of two numbers
+  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+*/
 
 
-//  function multiply (x, y, cb) {
-//   return cb(x*y)
-//  }
-
-// // multiply (2, 3, function(consume){
-// //   console.log(consume);
-// // });
-
-
-// function greeting (first, last, cb) {
-//   return cb('hello, ' + first + ' '+ last)
-//  }
-
-// // greeting ('john', 'doe', function(consume){
-// //   console.log(consume);
-// // });
-
-
-// /* Step 3: Check your work by un-commenting the following calls to consume(): */
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
-// // ==== Closures ==== 
+// ==== Closures ==== 
 
-// // Explain in your own words why `nestedfunction()` can access the variable `internal`.
+// Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// // Explanation: 
+// Explanation: Variable internal is a parent to nested function, so nested function can reach back to internal and pull data from there. 
 
 
 // const external = "I'm outside the function";
