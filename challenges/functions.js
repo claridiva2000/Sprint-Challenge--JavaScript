33d8580ee27e390b3d0a55b09a9747594cdc45ab
claridiva2000 <claridiva2000@gmail.com>
2019-01-26 15:37:12 -0600

    still working on it

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,21 +6,20 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function doHomework(subject, callback) {
-  console.log(`Starting my ${subject} homework.`);
-  callback();
-}
-function alertFinished(){
-  console.log('Finished my homework');
-}
 
-doHomework('math', alertFinished);
+function consume(x, y, cb) {
+  return cb(x-y);
+}
+consume(5, 3,function (minus){
+  console.log(minus);
+});
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
